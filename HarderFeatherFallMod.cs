69bbcd6cc4f61d00dbf45b322f3097c63d60bb27
Aron Filbert <afilbert@lyconic.com>
2023-07-21 22:31:15 -0500

    feat: Add stam drain and deadman's drop

--- HarderFeatherFallMod.cs
+++ HarderFeatherFallMod.cs
@@ -2,7 +2,6 @@
 using BepInEx.Configuration;
 using BepInEx.Logging;
 using HarmonyLib;
-using System.Collections.Generic;
 using UnityEngine;
 
 namespace ValheimMovementMods
@@ -13,11 +12,11 @@ namespace ValheimMovementMods
     {
         const string pluginGUID = "afilbert.ValheimHarderFeatherFallMod";
         const string pluginName = "Valheim - Harder Feather Fall Mod";
-        const string pluginVersion = "0.1.1";
+        const string pluginVersion = "0.2.0";
         public static ManualLogSource logger;
+        public static HarderFeatherFallMod plugin;
 
         private readonly Harmony _harmony = new Harmony(pluginGUID);
-        public static HarderFeatherFallMod _plugin;
 
         public static bool Started = false;
 
@@ -25,28 +24,37 @@ namespace ValheimMovementMods
         private static string WispLightName = "$item_demister";
         private static bool FeatherFallEnabled = false;
         private static bool WispLightEnabled = false;
+        private static bool PlayerFalling = false;
 
         private static float StaminaPercentage = 0;
         private static float InitialMaxFallSpeed = 0;
+        private static float StamDrainConversion = 0;
 
         public static ConfigEntry<bool> EnableToggle;
         public static ConfigEntry<bool> EnableEverywhere;
+        public static ConfigEntry<bool> AltMode;
+        public static ConfigEntry<int> AltStamDrain;
+        public static ConfigEntry<bool> HardcoreMode;
 
         void Awake()
         {
-            _plugin = this;
+            plugin = this;
             logger = Logger;
 
 
             _harmony.PatchAll();
             EnableToggle = Config.Bind<bool>("Mod", "Enable Mod", true, "Enable this mod");
-            EnableEverywhere = Config.Bind<bool>("Mod", "Override: Enable even when Wisplight equipped", false, "Enable even when Wisplight equipped");
+            EnableEverywhere = Config.Bind<bool>("Override", "Override: Enable even when Wisplight equipped", false, "Enable even when Wisplight equipped");
+            AltMode = Config.Bind<bool>("Alternative Mode", "Alternative: Uses stamina to stay aloft", false, "Uses stamina to stay aloft");
+            AltStamDrain = Config.Bind<int>("Alternative Mode", "Alternative: Stamina drain unit (larger drains faster)", 10, "Stamina drain unit");
+            HardcoreMode = Config.Bind<bool>("Hardcore Mode", "Hardcore: Wisplight bound and uses stamina", false, "Requires Wisplight for normal use and uses stamina to stay aloft");
+            StamDrainConversion = AltStamDrain.Value / 100;
         }
 
         [HarmonyPatch(typeof(Player), "SetControls")]
-        private class ToggleMovement
+        private class SetControlsPatch
         {
-            private static void Prefix(ref Player __instance, ref Vector3 movedir, ref bool run, ref bool autoRun, ref bool crouch, ref Vector3 ___m_lookDir, ref Vector3 ___m_moveDir, ref bool ___m_autoRun, ref bool ___m_crouchToggled, ref string ___m_actionAnimation, ref List<Player.MinorActionData> ___m_actionQueue)
+            private static void Prefix(ref Player __instance, ref Vector3 movedir, ref bool ___m_groundContact)
             {
                 if (!EnableToggle.Value)
                 {
@@ -58,6 +66,13 @@ namespace ValheimMovementMods
                 FeatherFallEnabled = slowFallEffect != null;
                 WispLightEnabled = whispLightEffect != null;
                 StaminaPercentage = __instance.GetStaminaPercentage();
+
+                PlayerFalling = __instance.GetVelocity().y < 0 && !___m_groundContact;
+
+                if (AltMode.Value && FeatherFallEnabled && PlayerFalling && (!WispLightEnabled || HardcoreMode.Value))
+                {
+                    __instance.UseStamina(StamDrainConversion);
+                }
             }
         }
 
@@ -76,6 +91,19 @@ namespace ValheimMovementMods
                     InitialMaxFallSpeed = ___m_maxMaxFallSpeed;
                 }
 
+                if (AltMode.Value && !HardcoreMode.Value)
+                {
+                    if (StaminaPercentage == 0)
+                    {
+                        ___m_maxMaxFallSpeed = 0;
+                    }
+                    else
+                    {
+                        ___m_maxMaxFallSpeed = 5;
+                    }
+                    return;
+                }
+
                 if (!WispLightEnabled || EnableEverywhere.Value)
                 {
                     if (StaminaPercentage != 0)
@@ -85,6 +113,11 @@ namespace ValheimMovementMods
                 } else
                 {
                     ___m_maxMaxFallSpeed = InitialMaxFallSpeed;
+                }
+
+                if (AltMode.Value && StaminaPercentage == 0)
+                {
+                    ___m_maxMaxFallSpeed = 0;
                 }
             }
         }
@@ -99,7 +132,21 @@ namespace ValheimMovementMods
                     return;
                 }
 
-                if (FeatherFallEnabled)
+                if (AltMode.Value && !HardcoreMode.Value)
+                {
+                    if (StaminaPercentage == 0)
+                    {
+                        ___m_fallDamageModifier = 0;
+                    } else
+                    {
+                        ___m_fallDamageModifier = -1;
+                    }
+                    return;
+                }
+
+                bool ApplyFeatherFall = (FeatherFallEnabled && (AltMode.Value && StaminaPercentage != 0)) || (FeatherFallEnabled && !AltMode.Value);
+
+                if (ApplyFeatherFall)
                 {
                     ___m_fallDamageModifier = -1;
                 }
@@ -110,7 +157,7 @@ namespace ValheimMovementMods
 
                 if (!WispLightEnabled || EnableEverywhere.Value)
                 {
-                    if (FeatherFallEnabled)
+                    if (ApplyFeatherFall)
                     {
                         float stamDiff = 1 - StaminaPercentage;
                         damage = 0;
