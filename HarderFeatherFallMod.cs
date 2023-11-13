0ed24e09d58bee8cc96c3448d7fece8880652d07
Aron Filbert <afilbert@lyconic.com>
2023-11-12 21:03:47 -0600

    feat: Complete v0.2.0 features
    fix: Fix potential multiple damage application
    
    fix: Fix damage application when disabling mod while midair

--- HarderFeatherFallMod.cs
+++ HarderFeatherFallMod.cs
@@ -2,6 +2,8 @@
 using BepInEx.Configuration;
 using BepInEx.Logging;
 using HarmonyLib;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
 
 namespace ValheimMovementMods
@@ -20,6 +22,9 @@ namespace ValheimMovementMods
 
         public static bool Started = false;
 
+        public static int FeatherFallStatusIndex = -1;
+        public static int AppliedDamageIndex = -1;
+
         private static string FeatherFallName = "$se_slowfall_name";
         private static string WispLightName = "$item_demister";
         private static bool FeatherFallEnabled = false;
@@ -27,8 +32,10 @@ namespace ValheimMovementMods
         private static bool PlayerFalling = false;
 
         private static float StaminaPercentage = 0;
-        private static float InitialMaxFallSpeed = 0;
-        private static float StamDrainConversion = 0;
+        private static float InitialMaxFallSpeed = 5f;
+        public static float ElapsedFreeFallTime = 0f;
+        public static float FullFallVelocity = 0f;
+        public static float CapturedFallVel = 0f;
 
         public static ConfigEntry<bool> EnableToggle;
         public static ConfigEntry<bool> EnableEverywhere;
@@ -41,38 +48,188 @@ namespace ValheimMovementMods
             plugin = this;
             logger = Logger;
 
-
             _harmony.PatchAll();
             EnableToggle = Config.Bind<bool>("Mod", "Enable Mod", true, "Enable this mod");
-            EnableEverywhere = Config.Bind<bool>("Override", "Override: Enable even when Wisplight equipped", false, "Enable even when Wisplight equipped");
-            AltMode = Config.Bind<bool>("Alternative Mode", "Alternative: Uses stamina to stay aloft", false, "Uses stamina to stay aloft");
-            AltStamDrain = Config.Bind<int>("Alternative Mode", "Alternative: Stamina drain unit (larger drains faster)", 10, "Stamina drain unit");
-            HardcoreMode = Config.Bind<bool>("Hardcore Mode", "Hardcore: Wisplight bound and uses stamina", false, "Requires Wisplight for normal use and uses stamina to stay aloft");
-            StamDrainConversion = AltStamDrain.Value / 100f;
+            EnableEverywhere = Config.Bind<bool>("Mod", "Hard mode: Enabled even when Wisplight equipped", false, "Stamina affects fall speed/damage even when Wisplight equipped");
+            AltMode = Config.Bind<bool>("Stamina Mode Override", "Drains stamina while aloft when Wisplight unequipped", false, "Uses stamina to stay aloft without Wisplight");
+            AltStamDrain = Config.Bind<int>("Stamina Mode Override", "Stamina drain unit (larger drains faster)", 10, "Stamina drain unit");
+            HardcoreMode = Config.Bind<bool>("Stamina Mode Override", "Hardcore mode: Drains stamina & affects fall speed/damage (only works if Stamina Mode Override enabled)", false, "Uses stamina while aloft, regardless of Wisplight, & affects fall speed/damage");
+        }
+
+        [HarmonyPatch(typeof(SEMan), "ModifyFallDamage")]
+        private class ModifyFallDamageSEManPatch
+        {
+            private static void Prefix(ref List<StatusEffect> ___m_statusEffects)
+            {
+                FeatherFallStatusIndex = ___m_statusEffects.FindIndex(se => se.m_name == FeatherFallName);
+                AppliedDamageIndex = 0;
+            }
+        }
+
+        [HarmonyPatch(typeof(SE_Stats), "ModifyFallDamage")]
+        private class ModifyFallDamagePatch
+        {
+            private static void Prefix(ref float baseDamage, ref float damage, ref float ___m_fallDamageModifier)
+            {
+                //logger.LogInfo($"featherfall index {FeatherFallStatusIndex} applied index {AppliedDamageIndex}");
+
+                // This bit of fun code is to track whether the caller was actually the Feather Fall Status effect
+                if (FeatherFallStatusIndex != AppliedDamageIndex)
+                {
+                    //logger.LogInfo($"rejecting AppliedDamageIndex {AppliedDamageIndex}");
+                    AppliedDamageIndex++;
+                    return;
+                }
+
+                FeatherFallStatusIndex = -1;
+                AppliedDamageIndex = 0;
+
+                float vel = FullFallVelocity;
+
+                //float ld = damage;
+                //float lbd = baseDamage;
+                // Don't need this, it's for performance tuning and logging
+                //float finalValue = ld += lbd * ___m_fallDamageModifier;
+
+                //logger.LogInfo($"final damage before {finalValue}");
+                //logger.LogInfo($"feather fall enabled? {FeatherFallEnabled} instantaneous vel {FullFallVelocity}");
+                if (!EnableToggle.Value || !FeatherFallEnabled)
+                {
+                    //logger.LogInfo("feather fall disabled (damage)");
+                    ___m_fallDamageModifier = FeatherFallEnabled ? -1 : 0;
+                    return;
+                }
+
+                //logger.LogInfo($"original damage modifier {___m_fallDamageModifier} original basedamage {baseDamage} damage {damage}");
+
+                if (AltMode.Value && !HardcoreMode.Value)
+                {
+                    setDamageModifier(ref damage, ref ___m_fallDamageModifier, vel);
+
+                    //ld = damage;
+                    //lbd = baseDamage;
+
+                    //float fv = ld += lbd * ___m_fallDamageModifier;
+
+                    //logger.LogInfo($"alt mode final damage {fv}");
+                    return;
+                }
+
+                ___m_fallDamageModifier = -1;
+                if (!WispLightEnabled || EnableEverywhere.Value || HardcoreMode.Value)
+                {
+                    //logger.LogInfo($"setting stam-derived damage {damage} baseDamage {baseDamage} modifier {___m_fallDamageModifier}");
+                    setStamDerivedDamage(ref damage, ref ___m_fallDamageModifier);
+                }
+
+                //ld = damage;
+                //lbd = baseDamage;
+
+                //finalValue = ld += lbd * ___m_fallDamageModifier;
+
+                //logger.LogInfo($"final damage after {finalValue}");
+            }
+
+            private static void setDamageModifier(ref float damage, ref float damageModifier, float vel)
+            {
+                //logger.LogInfo($"initial vel {vel}");
+                if (StaminaPercentage == 0)
+                {
+                    if (FullFallVelocity > 5.4f)
+                    {
+                        CapturedFallVel = Mathf.Clamp((vel * 2.9f), 0, 100);
+                        //logger.LogInfo($"captured fall vel {CapturedFallVel}");
+                        damage = CapturedFallVel;
+                        damageModifier = 0;
+                        CapturedFallVel = 0;
+                        FullFallVelocity = 0;
+                    }
+                    else
+                    {
+                        setFinalDamageModifier(ref damage, ref damageModifier);
+                    }
+                }
+                else
+                {
+                    setFinalDamageModifier(ref damage, ref damageModifier);
+                }
+            }
+
+            private static void setStamDerivedDamage(ref float damage, ref float damageModifier)
+            {
+                float stamDiff = 1 - StaminaPercentage;
+                damage = 0;
+                damageModifier = stamDiff;
+            }
+
+            private static void setFinalDamageModifier(ref float damage, ref float damageModifier)
+            {
+                if (HardcoreMode.Value)
+                {
+                    setStamDerivedDamage(ref damage, ref damageModifier);
+                }
+                else
+                {
+                    damageModifier = -1;
+                }
+            }
         }
 
         [HarmonyPatch(typeof(Player), "SetControls")]
         private class SetControlsPatch
         {
-            private static void Prefix(ref Player __instance, ref Vector3 movedir, ref bool ___m_groundContact)
+            private static void Prefix(ref Player __instance, ref bool ___m_groundContact, ref float ___m_fallTimer)
             {
+                Vector3 vel = __instance.GetVelocity();
+                StatusEffect slowFallEffect = __instance.GetSEMan().GetStatusEffects().Find(effect => effect.m_name == FeatherFallName);
+                StatusEffect whispLightEffect = __instance.GetSEMan().GetStatusEffects().Find(effect => effect.m_name == WispLightName);
+                FeatherFallEnabled = slowFallEffect != null;
+                WispLightEnabled = whispLightEffect != null;
+
                 if (!EnableToggle.Value)
                 {
                     return;
                 }
 
-                StatusEffect slowFallEffect = __instance.GetSEMan().GetStatusEffects().Find(effect => effect.m_name == FeatherFallName);
-                StatusEffect whispLightEffect = __instance.GetSEMan().GetStatusEffects().Find(effect => effect.m_name == WispLightName);
-                FeatherFallEnabled = slowFallEffect != null;
-                WispLightEnabled = whispLightEffect != null;
-                StaminaPercentage = __instance.GetStaminaPercentage();
-
                 PlayerFalling = __instance.GetVelocity().y < 0 && !___m_groundContact;
 
                 if (AltMode.Value && FeatherFallEnabled && PlayerFalling && (!WispLightEnabled || HardcoreMode.Value))
                 {
-                    __instance.UseStamina(StamDrainConversion);
+                    //logger.LogInfo($"using stam as falling with unit {AltStamDrain.Value / 100f}");
+                    __instance.UseStamina(AltStamDrain.Value / 100f);
                 }
+
+                StaminaPercentage = __instance.GetStaminaPercentage();
+
+                // Calculate fall time at 0 percent stam
+                if (AltMode.Value && PlayerFalling && FeatherFallEnabled && !__instance.IsFlying() && StaminaPercentage == 0)
+                {
+                    ElapsedFreeFallTime += Time.deltaTime;
+                }
+                else
+                {
+                    ElapsedFreeFallTime = 0f;
+                }
+
+                // Calculate fall velocity while in air
+                if (AltMode.Value && FeatherFallEnabled && PlayerFalling && ElapsedFreeFallTime > 0 && ___m_fallTimer > 0)
+                {
+                    int delay = Mathf.Abs(vel.y) > 50f ? 200 : 100;
+                    Task.Delay(delay).ContinueWith((task) => {
+                        setVelocity(vel);
+                    });
+                }
+
+                //if (vel.y < 0 && PlayerFalling)
+                //{
+                //    logger.LogInfo($"falling velocity {vel.y}");
+                //}
+            }
+
+            private static void setVelocity(Vector3 vel)
+            {
+                FullFallVelocity = Mathf.Abs(vel.y);
+                //logger.LogInfo($"setting delayed velocity at {FullFallVelocity}");
             }
         }
 
@@ -81,19 +238,20 @@ namespace ValheimMovementMods
         {
             private static void Prefix(ref Vector3 vel, ref float ___m_maxMaxFallSpeed)
             {
-                if (!EnableToggle.Value)
+                if (!EnableToggle.Value || !FeatherFallEnabled)
                 {
+                    ___m_maxMaxFallSpeed = FeatherFallEnabled ? 5 : 0;
                     return;
                 }
 
-                if (InitialMaxFallSpeed == 0)
+                if (WispLightEnabled)
                 {
-                    InitialMaxFallSpeed = ___m_maxMaxFallSpeed;
+                    ___m_maxMaxFallSpeed = 5;
                 }
 
                 if (AltMode.Value && !HardcoreMode.Value)
                 {
-                    if (StaminaPercentage == 0)
+                    if (StaminaPercentage <= 0)
                     {
                         ___m_maxMaxFallSpeed = 0;
                     }
@@ -104,65 +262,17 @@ namespace ValheimMovementMods
                     return;
                 }
 
-                if (!WispLightEnabled || EnableEverywhere.Value)
+                if (!WispLightEnabled || EnableEverywhere.Value || (AltMode.Value && HardcoreMode.Value))
                 {
                     if (StaminaPercentage != 0)
                     {
                         ___m_maxMaxFallSpeed = InitialMaxFallSpeed / StaminaPercentage;
                     }
-                } else
-                {
-                    ___m_maxMaxFallSpeed = InitialMaxFallSpeed;
                 }
 
-                if (AltMode.Value && StaminaPercentage == 0)
+                if (AltMode.Value && StaminaPercentage <= 0)
                 {
                     ___m_maxMaxFallSpeed = 0;
-                }
-            }
-        }
-
-        [HarmonyPatch(typeof(SE_Stats), "ModifyFallDamage")]
-        private class ModifyFallDamagePatch
-        {
-            private static void Prefix(float baseDamage, ref float damage, ref float ___m_fallDamageModifier, ref float ___m_maxMaxFallSpeed)
-            {
-                if (!EnableToggle.Value)
-                {
-                    return;
-                }
-
-                if (AltMode.Value && !HardcoreMode.Value)
-                {
-                    if (StaminaPercentage == 0)
-                    {
-                        ___m_fallDamageModifier = 0;
-                    } else
-                    {
-                        ___m_fallDamageModifier = -1;
-                    }
-                    return;
-                }
-
-                bool ApplyFeatherFall = (FeatherFallEnabled && (AltMode.Value && StaminaPercentage != 0)) || (FeatherFallEnabled && !AltMode.Value);
-
-                if (ApplyFeatherFall)
-                {
-                    ___m_fallDamageModifier = -1;
-                }
-                else
-                {
-                    ___m_fallDamageModifier = 0;
-                }
-
-                if (!WispLightEnabled || EnableEverywhere.Value)
-                {
-                    if (ApplyFeatherFall)
-                    {
-                        float stamDiff = 1 - StaminaPercentage;
-                        damage = 0;
-                        ___m_fallDamageModifier = stamDiff;
-                    }
                 }
             }
         }
